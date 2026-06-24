68a9d64aa6cc0f4a661dccb9d005f9e4fc5886ee
code-with-ricky <amrikdevofficial@gmail.com>
2026-06-25 00:20:33 +0530

    initial commit : hello world and simple types code done

---
+++ CH-1-Basics/p1_hello.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+    return `Hello, ${name}`;
+}
+
+const message: string = greet("Amrik");
+console.log(message);
---
+++ CH-1-Basics/p2_simple_types.ts
@@ -0,0 +1,17 @@
+// Basic Types -> primitves
+
+// Boolean -> true/false
+let isActive: boolean = false;
+let isAdmin = true; // typescript infers 'boolean' type
+
+// Number -> integer and floating point literals
+let age: number = 34;
+const pi: number = 3.14;
+
+// string -> text data
+// can use single quote (') or double quote (") or backticks (`)
+let username: string = "Ricky";
+let email: string = 'abc@gmail.com';
+let greetings: string = `Hello, ${username}`;
+
+
